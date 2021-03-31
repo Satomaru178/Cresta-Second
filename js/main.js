6e9c2818dbfd4dbe18570e2583c6e118f9fd5462
Satoshi yoshimura <63147677+Satomaru178@users.noreply.github.com>
2021-03-31 16:07:54 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@
 
     $win.on('load scroll', function () {
       var value = $(this).scrollTop();
-      if (value > mainPos/3) {
+      if (value > (mainPos/3 - 10)) {
         $header.addClass(changedClass);
       } else {
         $header.removeClass(changedClass);
